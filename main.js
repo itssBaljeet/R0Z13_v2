860e5a7668833a64df580875d6ec408e83eaa24a
itssBaljeet <noah.kendallh@gmail.com>
2024-04-18 16:27:54 -0400

    added voiceStateUpdate functionality

--- main.js
+++ main.js
@@ -21,12 +21,13 @@ const createWindow = () => {
 
 const client = new Client({
   intents: [
-   GatewayIntentBits.Guilds,
-   GatewayIntentBits.GuildMessages,
-   GatewayIntentBits.MessageContent,
-   GatewayIntentBits.GuildMembers,
- ],
-});
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates
+  ],
+})
 
 async function removeRole(role) {
   let guild = client.guilds.cache.get(process.env.SERVER_ID)
@@ -34,7 +35,7 @@ async function removeRole(role) {
   guild.members.cache.forEach(async (member) => {
     console.log(`${role} removed from ${member}`)
     await member.roles.remove(role)
-  });
+  })
 }
 
 async function startBot() {
@@ -54,12 +55,30 @@ app.whenReady().then(() => {
 
   // Discord Bot Starts Here
   startBot();
-  let removeJob = new CronJob('00 38 14 * * *', () => {
-    removeRole("1230574206312513537");
+  let removeJob = new CronJob('00 00 06 * * *', () => {
+    removeRole("1230574206312513537"); // manually input roleID
   })
   removeJob.start();
-  // Discord Bot Ends Here
 
+  client.on('voiceStateUpdate', (oldState, newState) => {
+    const txtChannel = client.channels.cache.get('1209245291128033340'); // manually input your own channel
+    const newChannelId = newState.channelId;
+    const oldChannelId = oldState.channelId;
+    if (oldChannelId === newChannelId) {
+      return;
+    }
+    if (oldChannelId === "1230588458968289361") { //manually put the voice channel ID
+      let role = newState.guild.roles.cache.get("1230574206312513537");
+      txtChannel.send(`${role} role removed from ${newState.member}`);
+      newState.member.roles.remove(role).catch(console.error);
+    }
+    else if (newChannelId === "1230588458968289361") {
+        console.log("GiveRole")
+        let role = oldState.guild.roles.cache.get("1230574206312513537");
+        txtChannel.send(`${role} role given to ${oldState.member}`);
+        oldState.member.roles.add(role).catch(console.error);
+    }
+  })
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
